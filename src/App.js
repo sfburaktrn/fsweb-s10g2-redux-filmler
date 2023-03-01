a7012635f0b0a18cfa3f7d45dda0e34a7b472f23
sfburaktrn <sbtorun.sbt@gmail.com>
2023-03-01 23:59:39 +0300

    done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-const App = props => {
+const App = (props) => {
   const displayFavorites = true;
 
   return (
